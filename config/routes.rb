afaf967182251fbd05967f78bb000eeaed015a3e
kuro <a1710dk@aiit.ac.jp>
2017-06-03 09:36:00 +0000

    finish 14

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,12 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :users
   resources :microposts, only: [:create, :destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :relationships, only: [:create, :destroy]
 end
