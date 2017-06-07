b7e9083d7d6fe2c2332187deb79805e8862cf456
kuro <a1710dk@aiit.ac.jp>
2017-06-07 17:06:21 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     has_many :microposts, dependent: :destroy
     has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
@@ -42,5 +43,34 @@ class User < ApplicationRecord
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
+    end
+    
+      # ランダムなトークンを返す
+    def User.new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    def remember
+      self.remember_token = User.new_token
+      update_attribute(:remember_digest, User.digest(remember_token))
+    end
+    
+    def self.digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def self.new_token
+      SecureRandom.urlsafe_base64
+    end
+    
+    def authenticated?(remember_token)
+      return false if remember_digest.nil?
+      BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
+    
+    def forget
+      update_attribute(:remember_digest, nil)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170603083516) do
+ActiveRecord::Schema.define(version: 20170607164922) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20170603083516) do
     t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
